99946d7cf33d0f3ac579d0673dbb7fde67e778d0
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-09-29 15:44:02 +0900

    バリエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :password, format: {with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i)}
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  
+
 end
+
