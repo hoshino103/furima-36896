b0d5c646a9c24182c65dfbd305af41399bffe06a
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 19:20:28 +0900

    FactoryBot user.emailにFakerを導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email             { 'test@example' }
+    email             {Faker::Internet.free_email}
     password          { 'Test12' }
     password_confirmation { password }
     name { 'test' }
