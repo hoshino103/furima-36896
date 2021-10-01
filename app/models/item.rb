1fbe77b98eacabc002263753f94a21ca3018950a
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 16:31:45 +0900

    新規商品登録のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,12 @@ class Item < ApplicationRecord
   # has_one :purchase_history
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :item_text, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
-
+  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
