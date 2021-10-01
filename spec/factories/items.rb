921b4b0484db50fa7436a9f4d4c11c850bcccae0
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 19:20:59 +0900

    FactoryBot itemを作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'test'}
+    item_text {'text'}
+    category_id {2}
+    status_id {2}
+    shipping_charge_id {2}
+    prefecture_id {2}
+    send_day_id {2}
+    price {300}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
