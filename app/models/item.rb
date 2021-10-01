ed22f83068fc00307ce86d01ffcd8a87fdf6d245
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 15:33:30 +0900

    修正とまだ使わないアソシエーションをコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :user_id
-  has_one :purchase_history
+  belongs_to :user
+  # has_one :purchase_history
+  has_one_attached :image
 
-  validates :title, :text, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
 
