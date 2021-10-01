e9d6b880ac5d05aba7c0b10d2f2c1aa5371298c2
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 19:22:29 +0900

    itemバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,10 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :item_text, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
-  validates :send_day_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :category_id, presence: true, numericality: { other_than: 1} 
+  validates :status_id, presence: true, numericality: { other_than: 1} 
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1} 
+  validates :prefecture_id, presence: true, numericality: { other_than: 0 } 
+  validates :send_day_id, presence: true, numericality: { other_than: 1} 
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
