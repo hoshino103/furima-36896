242ba69527911763b79908dd4f2b329d55dec849
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 14:04:00 +0900

    アクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user_id
   has_one :purchase_history
+
+  validates :title, :text, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
+
 end
