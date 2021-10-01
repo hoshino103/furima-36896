2142eecd55e6a9d6c574a8cbc07ef4eb0246f60e
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-01 12:19:51 +0900

    items_controllerにnewとcreateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(imte_params)
+  end
+
+  private
+
+  def imte_params
+    params.require(:item).permit(:name, :item_text, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :send_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
