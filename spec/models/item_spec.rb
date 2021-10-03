5683465340ef2e83f2d9a37268d8c4080566e680
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-03 15:03:27 +0900

    rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Item, type: :model do
       it '商品の状態の情報が--以外であること' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be other than 1")
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it '配送料の負担の情報が必須であること' do
         @item.shipping_charge_id = ''
@@ -66,7 +66,7 @@ RSpec.describe Item, type: :model do
       it '発送元の地域の情報が--以外であること' do
         @item.prefecture_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 0')
       end
       it '発送までの日数の情報が必須であること' do
         @item.send_day_id = ''
