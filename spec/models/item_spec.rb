ac72a676f0609f2078e1e6df0954f7286dcb2aac
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-03 15:26:01 +0900

    カテゴリーの情報が--以外であることの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,9 +34,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'カテゴリーの情報が--以外であること' do
-        @item.category_id = '--'
+        @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category is not a number')
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品の状態の情報が必須であること' do
         @item.status_id = ''
