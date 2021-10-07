ef1b4d715a4fd98e4612242d7ddfaecd68299420
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-07 12:37:15 +0900

    バリデーションとsaveメソッドの修正

--- app/models/bought_address.rb
+++ app/models/bought_address.rb
@@ -9,13 +9,12 @@ class BoughtAddress
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :municipalities
     validates :street_number
-    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/}
-    validates :purchase_history 
+    validates :phone_num, format: {with: /\A[0-9]{10,11}\z/} 
   end
 
   def save
     purchase_history = PurchaseHistory.create(item_id: item_id, user_id: user_id)
-    ShippingAddress.create(post_num: post_num, prefecture_id: prefecture_id, municipalities: municipalities, shipping_address: shipping_address, building_name: building_name, purchase_history_id: purchase_history.id)
+    ShippingAddress.create(post_num: post_num, prefecture_id: prefecture_id, municipalities: municipalities, street_number: street_number, building_name: building_name, phone_num: phone_num, purchase_history_id: purchase_history.id)
   end
 
 end
