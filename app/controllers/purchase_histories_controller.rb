a93687bb487b79350886d5385400c09a2ab5a8ae
hoshino103 <90019180+hoshino103@users.noreply.github.com>
2021-10-07 12:35:49 +0900

    コントローラーindexとcreateの修正

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,22 +1,24 @@
 class PurchaseHistoriesController < ApplicationController
   def index
     @bought_address = BoughtAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @bought_address = BoughtAddress.new(bought_address_params)
     if @bought_address.valid?
-      @bought_address.save
-      redirect_to root_path
+       @bought_address.save
+       redirect_to root_path
     else
-      render :index
+       @item = Item.find(params[:item_id])
+       render :index
     end
   end
 
   private
 
   def bought_address_params
-    params.require(:bought_address).permit(:post_num, :prefecture_id, :municipalities, :street_number, :bought_address, :phone_num).merge(user_id: current_user.id)
+    params.require(:bought_address).permit(:post_num, :prefecture_id, :municipalities, :street_number, :bought_address, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 
